2dcdd45fdf47c1dacd856577690adac3f16670f1
Stephen Moon <stephen.d.moon@gmail.com>
2024-11-27 14:00:01 -0500

    Implemented saving user to Redis cache

---
+++ src/shared/services/redis/user.cache.ts
@@ -0,0 +1,97 @@
+import { ServerError } from '@global/helpers/error-handler';
+import { config } from '@root/config';
+import { BaseCache } from '@services/redis/base.cache';
+import { IUserDocument } from '@user/interfaces/user.interface';
+import Logger from 'bunyan';
+
+const cacheName: string = 'userCache';
+const log: Logger = config.createLogger(cacheName);
+
+export class UserCache extends BaseCache {
+  constructor() {
+    super(cacheName);
+  }
+
+  public async saveUserToCache(key: string, userUId: string, createdUser: IUserDocument): Promise<void> {
+    const createdAt = new Date();
+    const {
+      _id,
+      uId,
+      username,
+      email,
+      avatarColor,
+      blocked,
+      blockedBy,
+      postsCount,
+      profilePicture,
+      followersCount,
+      followingCount,
+      notifications,
+      work,
+      location,
+      school,
+      quote,
+      bgImageId,
+      bgImageVersion,
+      social
+    } = createdUser;
+    const firstList: string[] = [
+      '_id',
+      `${_id}`,
+      'uid',
+      `${uId}`,
+      'username',
+      `${username}`,
+      'email',
+      `${email}`,
+      'avatarColor',
+      `${avatarColor}`,
+      'createdAt',
+      `${createdAt}`,
+      'postsCount',
+      `${postsCount}`
+    ];
+    const secondList: string[] = [
+      'blocked',
+      JSON.stringify(blocked),
+      'blockedBy',
+      JSON.stringify(blockedBy),
+      'profilePicture',
+      `${profilePicture}`,
+      'followersCount',
+      `${followersCount}`,
+      'followingCount',
+      `${followingCount}`,
+      'notifications',
+      JSON.stringify(notifications),
+      'social',
+      JSON.stringify(social)
+    ];
+    const thirdList: string[] = [
+      'work',
+      `${work}`,
+      'location',
+      `${location}`,
+      'school',
+      `${school}`,
+      'quote',
+      `${quote}`,
+      'bgImageVersion',
+      `${bgImageVersion}`,
+      'bgImageId',
+      `${bgImageId}`
+    ];
+    const dataToSave: string[] = [...firstList, ...secondList, ...thirdList];
+
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      await this.client.ZADD('user', { score: parseInt(userUId, 10), value: `${key}` });
+      await this.client.HSET(`users: ${key}`, dataToSave);
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error.  Try again.');
+    }
+  }
+}
