6d07f18e535ff7211150a248150376cd4aad7342
Stephen Moon <stephen.d.moon@gmail.com>
2025-01-08 16:17:44 -0500

    Added get reacions routes

--- src/features/reactions/routes/reactionRoutes.ts
+++ src/features/reactions/routes/reactionRoutes.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Add } from '@reactions/controllers/add-reaction';
 import { Remove } from '@reactions/controllers/remove-reaction';
+import { Get } from '@reactions/controllers/get-reactions';
 
 class ReactionRoutes {
   private router: Router;
@@ -11,7 +12,16 @@ class ReactionRoutes {
   }
 
   public routes(): Router {
+    this.router.get('/post/reactions/:postId', authMiddleware.checkAuthentication, Get.prototype.reactions);
+    this.router.get(
+      '/post/single/reaction/username/:postId/:username',
+      authMiddleware.checkAuthentication,
+      Get.prototype.singleReactionByUsername
+    );
+    this.router.get('/post/reactions/username/:username', authMiddleware.checkAuthentication, Get.prototype.reactionsByUsername);
+
     this.router.post('/post/reaction', authMiddleware.checkAuthentication, Add.prototype.reaction);
+
     this.router.delete(
       '/post/reaction/:postId/:previousReaction/:postReactions',
       authMiddleware.checkAuthentication,
