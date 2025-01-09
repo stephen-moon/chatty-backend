f8c845ff0a7fa656191dd7fa5f575231c3ca3de7
Stephen Moon <stephen.d.moon@gmail.com>
2025-01-09 09:22:58 -0500

    Added comment model schema

--- jest.config.ts
+++ jest.config.ts
@@ -26,6 +26,7 @@ const config: Config.InitialOptions = {
     '@user/(.*)': ['<rootDir>/src/features/user/$1'],
     '@post/(.*)': ['<rootDir>/src/features/post/$1'],
     '@reactions/(.*)': ['<rootDir>/src/features/reactions/$1'],
+    '@comments/(.*)': ['<rootDir>/src/features/comments/$1'],
     '@global/(.*)': ['<rootDir>/src/shared/globals/$1'],
     '@services/(.*)': ['<rootDir>/src/shared/services/$1'],
     '@sockets/(.*)': ['<rootDir>/src/shared/sockets/$1'],
