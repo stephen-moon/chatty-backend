a53d43c1fe4c61d82b6a8d4ef7cbaf7d6f39be46
Stephen Moon <stephen.d.moon@gmail.com>
2025-01-10 16:50:11 -0500

    Added unfollow-user controller

--- src/features/followers/routes/followerRoutes.ts
+++ src/features/followers/routes/followerRoutes.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Add } from '@followers/controllers/follower-user';
+import { Remove } from '@followers/controllers/unfollow-user';
 
 class FollowerRoutes {
   private router: Router;
@@ -11,6 +12,7 @@ class FollowerRoutes {
 
   public routes(): Router {
     this.router.put('/user/follow/:followeeId', authMiddleware.checkAuthentication, Add.prototype.follower);
+    this.router.put('/user/unfollow/:followeeId', authMiddleware.checkAuthentication, Remove.prototype.follower);
 
     return this.router;
   }
