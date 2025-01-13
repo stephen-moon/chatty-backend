4fc68df1d9e2e62676b9614123eb6710472cd779
Stephen Moon <stephen.d.moon@gmail.com>
2025-01-13 11:10:33 -0500

    Added get follow controller

---
+++ src/features/follows/controllers/get-follow.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+import { ObjectId } from 'mongodb';
+import HTTP_STATUS from 'http-status-codes';
+import mongoose from 'mongoose';
+import { FollowCache } from '@services/redis/follow.cache';
+import { IFollow, IFollowData } from '@follows/interfaces/follow.interface';
+import { followService } from '@services/db/follow.service';
+
+const followCache: FollowCache = new FollowCache();
+
+export class Get {
+  public async userFollowing(req: Request, res: Response): Promise<void> {
+    const userObjectId: ObjectId = new mongoose.Types.ObjectId(req.currentUser!.userId);
+    const cachedFollowees: IFollowData[] = await followCache.getFollowFromCache(`following:${req.currentUser!.userId}`);
+    const following: IFollow[] | IFollowData[] = cachedFollowees.length
+      ? cachedFollowees
+      : await followService.getFolloweeData(userObjectId);
+
+    res.status(HTTP_STATUS.OK).json({ message: 'User following', following });
+  }
+
+  public async userFollowers(req: Request, res: Response): Promise<void> {
+    const userObjectId: ObjectId = new mongoose.Types.ObjectId(req.params.userId);
+    const cachedFollowers: IFollowData[] = await followCache.getFollowFromCache(`followers:${req.params.userId}`);
+    const followers: IFollow[] | IFollowData[] = cachedFollowers.length
+      ? cachedFollowers
+      : await followService.getFollowerData(userObjectId);
+
+    res.status(HTTP_STATUS.OK).json({ message: 'User followers', followers });
+  }
+}
--- src/features/follows/routes/followRoutes.ts
+++ src/features/follows/routes/followRoutes.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Add } from '@follows/controllers/follow-user';
 import { Remove } from '@follows/controllers/unfollow-user';
+import { Get } from '@follows/controllers/get-follow';
 
 class FollowRoutes {
   private router: Router;
@@ -11,6 +12,8 @@ class FollowRoutes {
   }
 
   public routes(): Router {
+    this.router.get('/user/following', authMiddleware.checkAuthentication, Get.prototype.userFollowing);
+    this.router.get('/user/followers/:userId', authMiddleware.checkAuthentication, Get.prototype.userFollowers);
     this.router.put('/user/follow/:followeeId', authMiddleware.checkAuthentication, Add.prototype.follow);
     this.router.put('/user/unfollow/:followeeId', authMiddleware.checkAuthentication, Remove.prototype.follow);
 
