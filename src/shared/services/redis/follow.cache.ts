4f491ae2c98b94a25248ec1fe567ef591c928efb
Stephen Moon <stephen.d.moon@gmail.com>
2025-01-13 11:45:46 -0500

    Added block user in redis cache

--- src/shared/services/redis/follow.cache.ts
+++ src/shared/services/redis/follow.cache.ts
@@ -1,11 +1,13 @@
 import { IFollowData } from '@follows/interfaces/follow.interface';
 import { ServerError } from '@global/helpers/error-handler';
+import { Helpers } from '@global/helpers/helpers';
 import { config } from '@root/config';
 import { BaseCache } from '@services/redis/base.cache';
 import { UserCache } from '@services/redis/user.cache';
 import { IUserDocument } from '@user/interfaces/user.interface';
 import Logger from 'bunyan';
 import mongoose from 'mongoose';
+import { remove } from 'lodash';
 
 const cacheName: string = 'followCache';
 const log: Logger = config.createLogger(cacheName);
@@ -80,6 +82,30 @@ export class FollowCache extends BaseCache {
       }
 
       return list;
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error.  Try again.');
+    }
+  }
+
+  public async updateBlockedUserPropInCache(key: string, prop: string, value: string, type: 'block' | 'unblock'): Promise<void> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      const response: string = (await this.client.HGET(`users:${key}`, prop)) as string;
+      const multi: ReturnType<typeof this.client.multi> = this.client.multi();
+      let blocked: string[] = Helpers.parseJson(response) as string[];
+
+      if (type === 'block') {
+        blocked = [...blocked, value];
+      } else {
+        remove(blocked, (id: string) => id === value);
+      }
+
+      multi.HSET(`users:${key}`, `${prop}`, JSON.stringify(blocked));
+      await multi.exec();
     } catch (error) {
       log.error(error);
       throw new ServerError('Server error.  Try again.');
