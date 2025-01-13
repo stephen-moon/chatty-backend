870d106d03bad8313ee2723b1897e81f17721a71
Stephen Moon <stephen.d.moon@gmail.com>
2025-01-13 09:41:55 -0500

    Added get follow from redis cache

--- src/shared/services/redis/follow.cache.ts
+++ src/shared/services/redis/follow.cache.ts
@@ -1,10 +1,15 @@
+import { IFollowData } from '@follows/interfaces/follow.interface';
 import { ServerError } from '@global/helpers/error-handler';
 import { config } from '@root/config';
 import { BaseCache } from '@services/redis/base.cache';
+import { UserCache } from '@services/redis/user.cache';
+import { IUserDocument } from '@user/interfaces/user.interface';
 import Logger from 'bunyan';
+import mongoose from 'mongoose';
 
 const cacheName: string = 'followCache';
 const log: Logger = config.createLogger(cacheName);
+const userCache: UserCache = new UserCache();
 
 export class FollowCache extends BaseCache {
   constructor() {
@@ -44,6 +49,37 @@ export class FollowCache extends BaseCache {
       }
 
       await this.client.HINCRBY(`users:${userId}`, prop, value);
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error.  Try again.');
+    }
+  }
+
+  public async getFollowFromCache(key: string): Promise<IFollowData[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      const response: string[] = await this.client.LRANGE(key, 0, -1);
+      const list: IFollowData[] = [];
+      for (const item of response) {
+        const user: IUserDocument = (await userCache.getUserFromCache(item)) as IUserDocument;
+        const data: IFollowData = {
+          _id: new mongoose.Types.ObjectId(user._id),
+          username: user.username!,
+          avatarColor: user.avatarColor!,
+          postsCount: user.postsCount,
+          followersCount: user.followersCount,
+          followingCount: user.followingCount,
+          profilePicture: user.profilePicture,
+          uId: user.uId!,
+          userProfile: user
+        };
+        list.push(data);
+      }
+
+      return list;
     } catch (error) {
       log.error(error);
       throw new ServerError('Server error.  Try again.');
