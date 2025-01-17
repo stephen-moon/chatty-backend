d6e14acb817ea17b106ece9edf4b556d1c77e5f1
Stephen Moon <stephen.d.moon@gmail.com>
2025-01-17 16:55:30 -0500

    Added update-messages in redis cache

--- src/shared/services/redis/message.chache.ts
+++ src/shared/services/redis/message.chache.ts
@@ -1,6 +1,6 @@
 import { BaseCache } from '@services/redis/base.cache';
 import Logger from 'bunyan';
-import { find, findIndex } from 'lodash';
+import { find, findIndex, filter } from 'lodash';
 import { config } from '@root/config';
 import { ServerError } from '@global/helpers/error-handler';
 import { IChatList, IChatUsers, IGetMessageFromCache, IMessageData } from '@chats/interfaces/chat.interface';
@@ -159,6 +159,31 @@ export class MessageCache extends BaseCache {
       await this.client.LSET(`messages:${receiver.conversationId}`, index, JSON.stringify(chatItem));
       const lastMessage: string = (await this.client.LINDEX(`messages:${receiver.conversationId}`, index)) as string;
 
+      return Helpers.parseJson(lastMessage) as IMessageData;
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error.  Try again.');
+    }
+  }
+
+  public async updateChatMessages(senderId: string, receiverId: string): Promise<IMessageData> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      const userChatList: string[] = await this.client.LRANGE(`chatList:${senderId}`, 0, -1);
+      const receiver: string = find(userChatList, (listItem: string) => listItem.includes(receiverId)) as string;
+      const parsedReceiver: IChatList = Helpers.parseJson(receiver) as IChatList;
+      const messages: string[] = await this.client.LRANGE(`messages:${parsedReceiver.conversationId}`, 1, -1);
+      const unreadMessages: string[] = filter(messages, (listItem) => !Helpers.parseJson(listItem).isRead);
+      for (const [index, item] of unreadMessages.entries()) {
+        const chatItem = Helpers.parseJson(item) as IMessageData;
+        chatItem.isRead = true;
+        await this.client.LSET(`messages:${parsedReceiver.conversationId}`, index, JSON.stringify(chatItem));
+      }
+
+      const lastMessage: string = (await this.client.LINDEX(`messages:${parsedReceiver.conversationId}`, -1)) as string;
       return Helpers.parseJson(lastMessage) as IMessageData;
     } catch (error) {
       log.error(error);
